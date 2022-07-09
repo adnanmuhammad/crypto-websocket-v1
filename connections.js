09f52ab969baa1cc5164ae5df218a08361005abe
adnanmuhammad <adnanmalick@gmail.com>
2022-07-09 06:04:45 +0500

    commit on 9 july 2022

--- connections.js
+++ connections.js
@@ -1,9 +1,9 @@
 var mysql_obj = require('mysql');
 
 var db_connection = mysql_obj.createConnection({
-    host     : 'crypto-exchange.cvcoxaxglrwq.us-east-1.rds.amazonaws.com',
-    user     : 'admin',
-    password : 'sVEH0VWtkOgb7LjGc4A2',
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
     port: 3306, //port mysql
     database : 'cryptowebsocket'
 }, 'request');
